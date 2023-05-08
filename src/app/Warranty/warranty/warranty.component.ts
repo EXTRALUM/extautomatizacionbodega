2488f991210793d5aea16d4f0e9bd6f17fcb23dd
jsalvatierram <104515547+jsalvatierram@users.noreply.github.com>
2023-05-08 13:43:47 -0600

    Garantias v1.0

---
+++ src/app/Warranty/warranty/warranty.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit } from '@angular/core';
+import { warranty } from 'src/app/core/model/warranty.model';
+import { NumerodeSerie } from 'src/app/core/model/NumerodeSerie.model';
+import { WarrantyService } from 'src/app/core/service/warranty.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { LoginModel } from 'src/app/core/model/login.model';
+import { utiles } from 'src/environments/utiles';
+import { Console } from 'console';
+
+@Component({
+  selector: 'app-warranty',
+  templateUrl: './warranty.component.html',
+  styleUrls: ['./warranty.component.scss']
+})
+export class WarrantyComponent implements OnInit {
+  Modo: boolean;
+  NumeroDeSerieNuevo: string;
+  IndexArray: number;
+  loginModel = new LoginModel();
+  warrantyModel: warranty = new warranty();
+  warrantyInsertModel: warranty = new warranty();
+  vNumerodeSerieList: NumerodeSerie[] = [];
+  private unsubscribe$ = new Subject<void>();
+  vData: any;
+  constructor( 
+    private warrantyService: WarrantyService)
+     { }
+
+  ngOnInit(): void {
+    this.Modo = false;
+    this.NumeroDeSerieNuevo="";
+    
+    
+  }
+
+  getGarantia() {
+    if(this.warrantyModel.vCustInvoiceID !== ""){
+      this.loginModel = utiles.getCacheLogin();
+      this.warrantyModel.vColaboradorNombre = this.loginModel.UserId;
+      this.warrantyService.getGarantia(this.warrantyModel)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(
+          response => {
+            if (response) {
+              this.warrantyModel = Object.assign(response);
+            }
+          }
+        );
+    }
+  }
+
+  ModoCrear(){
+    
+    this.Modo = false;
+
+  }
+
+  ModoVer(){
+    
+    this.Modo = true;
+
+  }
+  InsertarGarantia(){
+    
+    if(this.warrantyModel.vCustInvoiceID !== ""){
+      this.loginModel = utiles.getCacheLogin();
+      this.ObtenerCantidadNS();
+      this.warrantyModel.vColaboradorNombre = this.loginModel.UserId;
+      this.warrantyService.insertGarantia(this.warrantyModel)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(
+        response => {
+        if (response) {
+            this.warrantyModel = Object.assign(response);
+          }
+        }
+      );
+    }
+
+  }
+
+  ObtenerCantidadNS(){
+
+    this.warrantyModel.vCantidadNS = this.warrantyModel.vListaNumeroDeSerie.length;  
+  }
+
+  AgregarNS(){
+    if(this.NumeroDeSerieNuevo !=="")
+    {
+      this.warrantyModel.vListaNumeroDeSerie.push({vNumeroDeSerie: this.NumeroDeSerieNuevo });
+      this.NumeroDeSerieNuevo ="";
+
+      this.ObtenerCantidadNS();
+    }
+  }
+
+  EliminarNS(NumerodeSerieList: NumerodeSerie){
+
+    this.IndexArray = this.warrantyModel.vListaNumeroDeSerie.indexOf(NumerodeSerieList);
+    /*this.warrantyModel.vListaNumeroDeSerie.splice(index,1);
+    
+    */console.log(this.IndexArray);
+    this.warrantyModel.vListaNumeroDeSerie = this.warrantyModel.vListaNumeroDeSerie.filter(item => item !== NumerodeSerieList);
+    this.ObtenerCantidadNS();
+  }
+
+  clearFactura() {
+    this.warrantyModel.vCustInvoiceID = '';
+    this.warrantyModel.vCantidadNS = 0;
+    this.warrantyModel.vClienteNombre ='';
+    this.warrantyModel.vColaboradorNombre ='';
+    this.warrantyModel.vListaNumeroDeSerie=[];
+    this.warrantyModel.vRefRecIDInvoice =0;
+    this.getGarantia();
+  }
+
+
+}
